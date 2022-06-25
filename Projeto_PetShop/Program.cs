a1443044ef9c63006be58d02effab0fe2098a799
Juddy Lopes <juddykelly.jk@gmail.com>
2022-06-25 09:25:00 -0300

    iniciando um novo projeto de um app para petshop

---
+++ Projeto_PetShop/Program.cs
@@ -0,0 +1,35 @@
+﻿
+
+
+
+using System.Text.RegularExpressions;
+
+namespace  Projeto_PetShop
+{
+    class Program
+    {
+        static void Main (string[] args)
+        {
+            //cadastro de numero de celular do cliente 
+            string padraoNumeroCelular = "[9][0-9]{8}";
+            string numeroCelularCliente = "Número de celular do cliente sem dígitos: 973426997";
+            if (Regex.IsMatch(numeroCelularCliente, padraoNumeroCelular))
+            {
+                Match numeroCorreto = Regex.Match(numeroCelularCliente, padraoNumeroCelular);
+                Console.WriteLine("Número cadastrado: " + numeroCorreto);
+            }
+            else
+            {
+                Console.WriteLine("Número de celular inválido.");
+            }
+
+
+
+
+
+
+
+        }
+
+    }
+}
